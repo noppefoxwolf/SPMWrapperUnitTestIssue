8c82a977b90c43a5e9f39e992839ca9434ca39be
noppe <tomoya.hirano@dena.com>
2022-01-21 15:32:03 +0900

    add app tests

---
+++ AppTests/AppTests.swift
@@ -0,0 +1,32 @@
+//
+//  AppTests.swift
+//  AppTests
+//
+//  Created by Tomoya Hirano on 2022/01/21.
+//
+
+import XCTest
+import SPM
+
+class AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        moduleBundle //safe
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
